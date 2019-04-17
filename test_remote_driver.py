a881434baa8df5e1d0041a89af86cd98e1d21cc5
srihari33 <yvsriharikrishna3@gmail.com>
2019-04-17 16:11:30 +0530

    Update test_remote_driver.py

--- test_remote_driver.py
+++ test_remote_driver.py
@@ -8,14 +8,28 @@ capabilities = DesiredCapabilities.INTERNETEXPLORER.copy()
 
 
 capabilities['platform'] = "windows"
-capabilities['build'] = 12001
+#capabilities['INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS'] = True
+#capabilities['INITIAL_BROWSER_URL'] = 'https://www.google.com/'
+# capabilities['build'] = 12001
 
 # Instantiate an instance of Remote WebDriver with the desired capabilities.
 driver = webdriver.Remote(desired_capabilities=capabilities,
                           command_executor=selenium_grid_url)
 
-driver.get("http://example.com")
-driver.implicitly_wait(10)
+# driver.implicitly_wait(10)
 import time
-time.sleep(8)
+driver.get("https://www.google.com/")
+time.sleep(5)
+elems = driver.find_elements_by_xpath("//input[contains(@name, 'q')]")
+iframe = driver.find_elements_by_xpath("/iframe")
+print('Iframe ===== {}'.format(iframe))
+print("Elems  ===== {}".format(elems))
+elems[0].send_keys("testing")
+driver.find_element_by_xpath("//input[contains(@value, 'Google Search')]").click()
+driver.save_screenshot("test.png")
+handle = driver.window_handles.index(driver.current_window_handle)
+print('Handle ==== {}'.format(handle))
+
 driver.quit()
+
+
